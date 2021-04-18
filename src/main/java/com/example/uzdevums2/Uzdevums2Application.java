b3c57270a9c098b343726dd3c9e3d3748c722092
Spagetti-John <78023444+Spagetti-John@users.noreply.github.com>
2021-04-18 10:27:43 +0300

    add spring project

---
+++ src/main/java/com/example/uzdevums2/Uzdevums2Application.java
@@ -0,0 +1,13 @@
+package com.example.uzdevums2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uzdevums2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Uzdevums2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/uzdevums2/Uzdevums2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uzdevums2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uzdevums2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
